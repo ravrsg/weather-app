444ddb8f5e50f045c28437e65cedb99a5289a90e
RaV <rafal.jakuczek@hotmail.com>
2022-08-13 15:55:29 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  #app.center
+    <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Secular+One&display=swap');
+#app {
+  font-family: 'Secular One', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  width: 640px;
+  height: 480px;
+}
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    apikey: '027ee07fafd9a678d925c3a9220c1289',
+    data: [],
+    error: ''
+  },
+  actions: {
+    async getData ({ commit }, city) {
+      const axios = require('axios').default;
+
+      await axios({
+        method: 'post',
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + this.state.apikey + "&lang=pl&units=metric",
+      })
+      .then(function (response) {
+        commit('setData', response);
+        commit('setError', '');
+      })
+      .catch(function (error) {
+        commit('setError', error.response.data.message);
+        commit('setData', []);
+      })
+    },
+    async getDataByLocation ({ commit }, crd) {
+      const axios = require('axios').default;
+      await axios({
+        method: 'post',
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + crd.latitude + '&lon=' + crd.longitude + '&appid=' + this.state.apikey + "&lang=pl&units=metric",
+      })
+      .then(function (response) {
+        commit('setData', response);
+        commit('setError', '');
+      })
+      .catch(function (error) {
+        commit('setError', error.response.data.message);
+        commit('setData', []);
+      })
+    }
+  },
+  mutations: {
+    setData (state, data) {
+      state.data = data;
+    },
+    setError (state, data) {
+      state.error = data;
+    }
+  },
+});
